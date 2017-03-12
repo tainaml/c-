c4311f47e5ab80a64f4bc53bd4f648e4ab2b1936
tainaml <tainaml@live.ca>
2017-03-12 18:37:57 -0300

    w3resource question 2

---
+++ questao2/questao2/Program.cs
@@ -0,0 +1,78 @@
+﻿/*2. Write a program in C# Sharp to read n number of values in an array and display 
+it in reverse order. Go to the editor
+Test Data : 
+Input the number of elements to store in the array :3  ok
+Input 3 number of elements in the array : 
+element - 0 : 2 
+element - 1 : 5 
+element - 2 : 7 
+Expected Output : 
+The values store into the array are : 
+2 5 7 
+The values store into the array in reverse are : 
+7 5 2 
+
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace questao2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("type the number of elements you want: ");
+            int numberOfElements = Convert.ToInt16((Console.ReadLine()));
+
+
+            int[] a1 = new int[numberOfElements];
+
+            int i = 0;
+            
+
+            while (i < numberOfElements)
+                {
+
+                Console.WriteLine("choose a type to add: ");
+                int x = Convert.ToInt16(Console.ReadLine());
+                a1[i]  = x;
+                i++;               
+            }
+
+
+            for (i=0; i < a1.Length; i++)
+            {
+                Console.WriteLine($"element {i} valor armazenado pelo usuario: {a1[i]}");
+
+            }
+           
+
+
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
+
+
