29c8de3365b5bef6c82c42965498f5d46809c33a
tainaml <tainaml@live.ca>
2017-03-22 13:21:14 -0300

    ex

---
+++ List/List/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa();
+            Pessoa p2 = new Pessoa();
+            Pessoa p3 = new Pessoa();
+            List<Pessoa> lista = new List<Pessoa>();
+
+            lista.Add(p1);
+            lista.Add(p2);
+            lista.Add(p3);
+
+            int qtd = lista.Count();
+
+            Console.WriteLine(qtd);
+
+            Console.ReadLine();
+                
+
+
+
+
+        }
+    }
+}
